8b863ba2cd5a0fa22f5ef6b85a03ab34f387cddd
Tounsi Asmaa <tasmaa2909@gmail.com>
2022-06-25 19:14:42 +0100

    9th commit

--- src/main/java/org/sid/ebankingbackend/web/BankAccountRestAPI.java
+++ src/main/java/org/sid/ebankingbackend/web/BankAccountRestAPI.java
@@ -1,8 +1,7 @@
 package org.sid.ebankingbackend.web;
 
-import org.sid.ebankingbackend.dtos.AccountHistoryDTO;
-import org.sid.ebankingbackend.dtos.AccountOperationDTO;
-import org.sid.ebankingbackend.dtos.BankAccountDTO;
+import org.sid.ebankingbackend.dtos.*;
+import org.sid.ebankingbackend.exceptions.BalanceNotSufficientException;
 import org.sid.ebankingbackend.exceptions.BankAccountNotFoundException;
 import org.sid.ebankingbackend.services.BankAccountService;
 import org.springframework.web.bind.annotation.*;
@@ -32,5 +31,22 @@ public class BankAccountRestAPI {
                                                @RequestParam(name="page",defaultValue = "0") int page ,
                                                @RequestParam(name="size",defaultValue = "5") int size) throws BankAccountNotFoundException {
         return bankAccountService.getAccountHistory(accountId,page,size);
+    }
+    @PostMapping("/accounts/debit")
+    public DebitDTO debit(@RequestBody DebitDTO debitDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.debit(debitDTO.getAccountId(),debitDTO.getAmount(),debitDTO.getDescription());
+        return debitDTO;
+    }
+    @PostMapping("/accounts/credit")
+    public CreditDTO credit(@RequestBody CreditDTO creditDTO) throws BankAccountNotFoundException {
+        this.bankAccountService.credit(creditDTO.getAccountId(),creditDTO.getAmount(),creditDTO.getDescription());
+        return creditDTO;
+    }
+    @PostMapping("/accounts/transfer")
+    public void transfer(@RequestBody TransferRequestDTO transferRequestDTO) throws BankAccountNotFoundException, BalanceNotSufficientException {
+        this.bankAccountService.transfer(
+                transferRequestDTO.getAccountSource(),
+                transferRequestDTO.getAccountDestination(),
+                transferRequestDTO.getAmount());
     }
 }
